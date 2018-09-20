737d5a47ae26517045d643b7d4262c497a140f3a
s135755 <j.mols@student.tue.nl>
2018-09-20 14:20:05 +0200

    Yellow pegs
    Pegs also don't cast shadows on each other anymore

---
+++ Assets/Yellow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Yellow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Renderer>().material.color = Color.yellow;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
