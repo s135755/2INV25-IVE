f62c3c62e0bc9f203245f83fe579c0d334805a84
Jorik Mols <j.mols@student.tue.nl>
2018-09-21 13:00:16 +0200

    Added control script

---
+++ Assets/ExperimentControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperimentControl : MonoBehaviour {
+
+	// Time parameters
+	public static int INTROTIME = 5;
+	public static int EXPTIME = 10;
+
+	// Different states
+	public enum State {INTRO, EXP, FINISH};
+
+	// Keep track of time and state
+	public double elapsedTime;
+	public State state;
+
+	// Use this for initialization
+	void Start () {
+		elapsedTime = 0;
+		state = State.INTRO;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Increment time counter
+		elapsedTime += Time.deltaTime;
+
+		// TODO: for each state, do stuff
+		switch(state)
+		{
+			case State.INTRO:
+				// Show dialog with experiment explanation maybe?
+				break;
+			case State.EXP:
+				// Construct a certain number of alignment tests and show them
+				break;
+			case State.FINISH:
+				// Remove everything from display, maybe display dialogue
+				break;
+		}
+	}
+}
