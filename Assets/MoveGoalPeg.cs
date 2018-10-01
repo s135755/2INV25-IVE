0502e95d7930650e414f746e8a4fe805c7b60439
Jorik Mols <j.mols@student.tue.nl>
2018-10-01 18:59:58 +0200

    Changed peg movement
    Now dependant on plane, which is easier to change.

--- Assets/MoveGoalPeg.cs
+++ Assets/MoveGoalPeg.cs
@@ -9,9 +9,12 @@ public class MoveGoalPeg : MonoBehaviour {
     float x, y, z;
     double movementFactor;
 
+    // Goal plane
+    public GameObject plane;
+
     void Start()
     {
-        //Set y and movement factor constants
+        // Set y and movement factor constants
         y = transform.position.y;
         movementFactor = 1;
     }
@@ -20,20 +23,14 @@ public class MoveGoalPeg : MonoBehaviour {
     void Update()
     {
 
-        //Get mouse Position
-        var mousePos = Input.mousePosition;
-        //Transform position             
-        var wantedPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10));
-
-        //Get Schroll movement
-        var d = Input.GetAxis("Mouse ScrollWheel");
-
-        //Set x and z positions
-        x = wantedPos.x;
-        z = transform.position.z + (float)(d*movementFactor);
+        // Get mouse Position
+        Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         
-        //Move the peg
-        transform.position = new Vector3(x, y, z);
+        // Get new peg position
+        Vector3 pegPos = plane.GetComponent<PlaneFunctions>().getPegPointByMouseLocation(mousePos);
+        
+        // Move the peg
+        transform.position = pegPos;
         
     }
 }
---
+++ Assets/PlaneFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneFunctions : MonoBehaviour{
+
+	// Bottom left corner point as 
+	// Get a point for the peg for the given mouse location
+	public Vector3 getPegPointByMouseLocation(Vector2 mouseLocation) {
+		
+		// Get bottom left point
+		Vector3 planeCenter = this.transform.position;
+		Vector3 extent = GetComponent<Renderer>().bounds.extents;
+		Vector3 bottomLeft = new Vector3(planeCenter.x - extent.x, planeCenter.y, planeCenter.z - extent.z);
+
+		// Get relative position of mouse on the plane
+		// Scale from screen size to plane size
+		float w = extent.x * 2;
+		float h = extent.z * 2;
+		float pegX = bottomLeft.x + (w / Screen.width) * mouseLocation.x;
+		float pegZ = bottomLeft.z + (h / Screen.height) * mouseLocation.y;
+
+		return new Vector3(pegX, planeCenter.y, pegZ);
+	}
+}
