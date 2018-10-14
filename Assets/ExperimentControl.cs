dc4322d86429cb6fc7d185ca0f0ed6979e83ce97
Luis <luis.villap@alumnos.upm.es>
2018-10-14 16:24:38 +0200

    Peg placement, text on scrin, data storage, test and experiment coun

--- Assets/ExperimentControl.cs
+++ Assets/ExperimentControl.cs
@@ -1,20 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEngine.UI;
 
 public class ExperimentControl : MonoBehaviour {
 
-	// Time and experiment parameters
-	public static int INTROTIME = 5;
-	public static int TIMEBETWEENEXP = 2;
+    // Time and experiment parameters
+    public static int INTROTIME = 5;
+    public static int TIMEBETWEENEXP = 4;
     public static int NUMEXP = 10;
+    public static int NUMTEST = 3;
 
+    //Experiment counter
+    int elapsedTests;
+    //Data storag path relative to assets
+    string relativePath = "/Resources/ExperimentData.txt";
+    //Data storage path
+    string path;
+
+    //IOD and FOV values
+    float iod, fov;
+
+    //BlackScreen text
+    public Text bsText;
 
     // Flag for storing data
-    private bool STOREDATA = true;
+    private bool STOREDATA = false;
 
-	// Different states
-	public enum State {INTRO, EXPIDLE, EXPRUNNING, FINISH};
+    // Different states
+    public enum State { INTRO, EXPIDLE, EXPRUNNING, FINISH, WAITING };
 
 	// Keep track of time and state of experiments
 	public State state;
@@ -23,10 +39,17 @@ public class ExperimentControl : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        
         startTime = Time.time;
         elapsedExp = 0;
 		state = State.INTRO;
-	}
+        path = Application.dataPath + relativePath;
+        //Since we are doing 3 training tests it will have values -2,-1 and 0 for them
+        elapsedTests = 0;
+        bsText = GameObject.Find("bsText").GetComponent<Text>();
+        bsText.text = "This is Training Test " + (elapsedTests + 1);
+        deleteDataFile();
+    }
 
     // Update is called once per frame
     void Update()
@@ -40,6 +63,12 @@ public class ExperimentControl : MonoBehaviour {
                     state = State.EXPIDLE;
                 }
                 break;
+            case State.WAITING:
+                if (Time.time - startTime >= TIMEBETWEENEXP)
+                {
+                    state = State.EXPIDLE;
+                }
+                break;
             case State.EXPIDLE:
                 if (elapsedExp == NUMEXP) {
                     state = State.FINISH;
@@ -50,9 +79,12 @@ public class ExperimentControl : MonoBehaviour {
             case State.EXPRUNNING:
                 if (Input.GetMouseButtonDown(0))
                     {
-                        // TODO: obtain data
-                        finishTest(new Vector2(0,0), 0, 0, 0);
+                     if (elapsedTests >= NUMTEST)
+                        STOREDATA = true;
+
+                     finishTest(startTime,iod, fov);
                     }
+                    
                 break;
             case State.FINISH:
                 blackScreen();
@@ -61,24 +93,43 @@ public class ExperimentControl : MonoBehaviour {
     }
 
     // Finishes the test by storing all the given data
-    void finishTest(Vector2 pegDist, float time, float iod, float fov)
+    void finishTest(float startTime, float iod, float fov)
     {
-        blackScreen();
+        // Keep count of tests
+        if (elapsedTests < NUMTEST)
+            elapsedTests++;
+        
         if (STOREDATA) {
-            // TODO : I/O  all data to text file
+            float elapsedTime = Time.time - startTime;
+           //Getting the variables, preparing the JSON and calling StoreData
+            Vector2 distBetweenPegs = PegControl.getDistanceBetweenPegs();
+            object[] args = new object[] { elapsedExp + 1, distBetweenPegs.x, distBetweenPegs.y, elapsedTime, fov, iod };
+            string newData = "\"ExperimentData\": { \"Experiment\": " + args[0] + ", " +
+                "\"XDistance\": " + args[1] +", " +
+                "\"ZDistance\": " + args[2] + ", " +
+                "\"Time\": " + args[3] + ", " +
+                "\"FOV\": " + args[4] + ", " +
+                "\"IOD\": " + args[5] + " }";/**/
+            StoreData(newData);
+            elapsedExp++;
         }
+        blackScreen();
 
-        // Leave screen black for some amount of time
-        wait(TIMEBETWEENEXP);
-        state = State.EXPIDLE;
+        //For the black screen to wait
+        startTime = Time.time;
+        state = State.WAITING;
     }
 
     // Sets up and starts the next test
     void startTest()
     {
         // TODO : Set different FOV and IOD values
-        // TODO : Put plegs in correct places
+        fov = Random.Range(-5, 5);
+        iod = Random.Range(30, 180);
         
+        // Put plegs in correct places
+        PegControl.setPegsRandomly();
+
         // Set state to running and record starting time of test
         state = State.EXPRUNNING;
         startTime = Time.time;
@@ -90,6 +141,17 @@ public class ExperimentControl : MonoBehaviour {
     // Puts the screen to black
     void blackScreen()
     {
+
+        //Screen text message
+        if (elapsedTests < NUMTEST )
+            bsText.text = "This is Training Test " + (elapsedTests+1);
+        else
+        {
+            if (elapsedExp < NUMEXP)
+                bsText.text = "This is Experiment " + (elapsedExp+1);
+            else
+                bsText.text = "END";
+        }
         //Turning screen black by switching from normal camara to black screen camara        
         GameObject.FindWithTag("MainCamera").GetComponent<Camera>().enabled = false;
         GameObject.FindWithTag("CameraTwo").GetComponent<Camera>().enabled = true;
@@ -98,14 +160,43 @@ public class ExperimentControl : MonoBehaviour {
     // Puts the screen to normal
     void normalScreen()
     {
+        
+        bsText.text = ""; 
         //Turning screen from black to normal 
         GameObject.FindWithTag("MainCamera").GetComponent<Camera>().enabled = true;
         GameObject.FindWithTag("CameraTwo").GetComponent<Camera>().enabled = false;
     }
 
-    //Wait function to sleep thread
-    IEnumerator wait(int seconds)
+    //Function to store the data of the experiment
+    void StoreData(string newData)
     {
-        yield return new WaitForSeconds(seconds);
+        string data = "";
+        
+        //If the file exists already we save the current data
+        if (System.IO.File.Exists(path))
+        {
+            StreamReader inp_stm = new StreamReader(path);
+            while (!inp_stm.EndOfStream)
+            {
+                string inp_ln = inp_stm.ReadLine().ToString();
+                data += inp_ln + "\r\n";
+            }
+
+            inp_stm.Close();
+        }
+        string finalData = data + newData + "\r\n";
+        System.IO.File.WriteAllText(path, finalData);
+    }
+
+   void deleteDataFile()
+    {
+        if (System.IO.File.Exists(path))
+        {
+            System.IO.File.Delete(path);
+        }
+        if (System.IO.File.Exists(path+".meta"))
+        {
+            System.IO.File.Delete(path+".meta");
+        }
     }
 }
--- Assets/PegControl.cs
+++ Assets/PegControl.cs
@@ -4,40 +4,32 @@ using UnityEngine;
 
 public class PegControl : MonoBehaviour {
 
-    //X variable range from center
-    int xRange = 5;
-    //X variable range from center
-    int zRange = 5;
-
-    //Variables to store Target and Goal planes position
-    Vector3 TargetPosition, GoalPosition;
-
+    
     //Peg position variables
-    float x, y, z;
-    double movementFactor;
+    public float x, y, z;
 
     // Gameobjects
-    public GameObject goalPlane, targetPlane;
-    public GameObject targetPeg, goalPeg;
+    public static GameObject goalPlane, targetPlane;
+    public static GameObject targetPeg, goalPeg;
 
     //Function to get a random position within the planes
-    Vector3 getRandomPos(Vector3 position)
+    public static Vector3 getRandomPos(GameObject plane)
     {
-        float x = Random.Range(-xRange, xRange);
+        Vector3 planeCenter = plane.transform.position;
+        Vector3 extent = plane.GetComponent<Renderer>().bounds.extents;
+        Vector3 position = new Vector3(extent.x/2, planeCenter.y, extent.z/2);
+
+        float x = Random.Range(-position.x, position.x);
         float y = position.y;
-        float z = Random.Range(-zRange, zRange);
+        float z = Random.Range(-position.z, position.z);
         return new Vector3(x, y, z);
-    }
-    //Function to set the values for the planes position
-    void setPlanePosition()
-    {
-            TargetPosition = targetPlane.transform.position;
-            GoalPosition = goalPlane.transform.position;
     }
 
     //Function that returns the X and  Z distance between two pegs
-    public Vector2 getDistanceBetweenPegs()
+    public static Vector2 getDistanceBetweenPegs()
     {
+       // GameObject goalPeg = GameObject.Find("GoalPeg");
+       // GameObject targetPeg = GameObject.Find("TargetPeg");
         float xdist = Mathf.Abs(goalPeg.transform.position.x - targetPeg.transform.position.x);
         float zdist = Mathf.Abs(goalPeg.transform.position.z - targetPeg.transform.position.z);
         return new Vector2(xdist,zdist);
@@ -56,18 +48,22 @@ public class PegControl : MonoBehaviour {
     void Start () {
 
         assignGameObjects();
-
-        setPlanePosition();
+        
         
         //Randomly set the peg starting position
-        targetPeg.transform.position = getRandomPos(TargetPosition);
-        goalPeg.transform.position = getRandomPos(GoalPosition);
+        targetPeg.transform.position = getRandomPos(targetPlane);
+        goalPeg.transform.position = getRandomPos(goalPlane);
 
         // Set y and movement factor constants
         y = transform.position.y;
-        movementFactor = 1;
     }
 	
+    public static void setPegsRandomly()
+    {
+        //Randomly set the peg starting position
+        targetPeg.transform.position = getRandomPos(targetPlane);
+        goalPeg.transform.position = getRandomPos(goalPlane);
+    }
 	// Update is called once per frame
 	void Update () {
         // Get mouse Position
@@ -78,5 +74,10 @@ public class PegControl : MonoBehaviour {
 
         // Move the peg
         goalPeg.transform.position = pegPos;
+    }
+    public static void hidePegs(bool hide)
+    {
+        goalPeg.SetActive(!hide);
+        targetPeg.SetActive(!hide);
     }
 }
